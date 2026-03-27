e48adc8a1885d4fe3c22dbe2988a431f73e1369d
nikkiai0024 <nikki.ai.0024@gmail.com>
2026-03-28 01:13:48 +0900

    chore: v1.0.2 build 7 (YouTube 403修正・Criticalバグ修正版)

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ module.exports = {
   expo: {
     name: "ミルレシピ",
     slug: "miru-recipe",
-    version: "1.0.1",
+    version: "1.0.2",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -18,7 +18,7 @@ module.exports = {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.mirurecipe.app",
-      buildNumber: "5",
+      buildNumber: "7",
       privacyPolicyUrl: "https://github.com/nikkiai0024/miru-recipe/blob/main/PRIVACY.md",
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
